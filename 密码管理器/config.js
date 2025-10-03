51f481df87efba4726bae90bf885794121510562
Bejohnself <3573256829@qq.com>
2025-10-03 11:55:02 +0800

    测试6：修改跳转uri

--- 密码管理器/config.js
+++ 密码管理器/config.js
@@ -3,7 +3,7 @@ const MASTER_PASSWORD_KEY = 'master_password_hash';
 const PASSWORDS_KEY = 'encrypted_passwords';
 const THEME_KEY = 'preferred_theme';
 const CUSTOM_KEYS_KEY = 'custom_keys'; // 新增自定义快捷键存储键
-const REDIRECT_URI = "https://bejohnself.github.io/Tools/dropbox.html"; // 必须和Dropbox后台一致
+const REDIRECT_URI = "https://bejohnself.github.io/Tools/密码管理器/"; // 必须和Dropbox后台一致
 
 // 当前会话状态
 let isAuthenticated = false;
