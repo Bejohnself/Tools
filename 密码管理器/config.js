26aa0ce489233e3feab9e487a15d2c1a27e34ca3
Bejohnself <3573256829@qq.com>
2025-10-03 12:49:53 +0800

    测试7：更改重定向链接（去除中文字符）

--- 密码管理器/config.js
+++ 密码管理器/config.js
@@ -3,7 +3,7 @@ const MASTER_PASSWORD_KEY = 'master_password_hash';
 const PASSWORDS_KEY = 'encrypted_passwords';
 const THEME_KEY = 'preferred_theme';
 const CUSTOM_KEYS_KEY = 'custom_keys'; // 新增自定义快捷键存储键
-const REDIRECT_URI = "https://bejohnself.github.io/Tools/密码管理器/"; // 必须和Dropbox后台一致
+const REDIRECT_URI = "https://bejohnself.github.io/Tools/%E5%AF%86%E7%A0%81%E7%AE%A1%E7%90%86%E5%99%A8/"; // 必须和Dropbox后台一致
 
 // 当前会话状态
 let isAuthenticated = false;
